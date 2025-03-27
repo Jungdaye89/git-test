2469d8198cbc699a9f125138b33c09b417815d95
JUNG DAYE <jungyj0809@gmail.com>
2025-03-27 20:00:21 +0900

    scenario2 base

--- git/src/main/java/com/example/git/TestController.java
+++ git/src/main/java/com/example/git/TestController.java
@@ -16,4 +16,13 @@ public class TestController {
 
         return ResponseEntity.ok("git 충돌 연습하기");
     }
+
+    @GetMapping("/scenario2")
+    public ResponseEntity<String> scenario2() {
+
+        System.out.println("깃 충돌");
+        System.out.println("연습 중입니다");
+
+        return ResponseEntity.ok("같은 파일에서 다른 줄 수정할 때 충돌");
+    }
 }
