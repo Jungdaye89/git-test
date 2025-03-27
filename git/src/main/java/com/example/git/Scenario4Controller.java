c0b005fcac274a88e3f4518442bc0fd11dbae4e7
JUNG DAYE <jungyj0809@gmail.com>
2025-03-27 22:13:36 +0900

    scenario4 base

---
+++ git/src/main/java/com/example/git/Scenario4Controller.java
@@ -0,0 +1,19 @@
+package com.example.git;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/scenario4")
+public class Scenario4Controller {
+
+    @GetMapping
+    public ResponseEntity<String> scenario4() {
+
+        return ResponseEntity.ok("scenario4 base");
+    }
+}
